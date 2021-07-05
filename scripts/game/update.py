1d39cff721810d113d36a24111e65345afee220e
CamilleFerros <80734960+CartmanORCamille@users.noreply.github.com>
2021-07-05 11:14:39 +0800

    修复重复更新bug
    Signed-off-by: CamilleFerros <80734960+CartmanORCamille@users.noreply.github.com>

--- scripts/game/update.py
+++ scripts/game/update.py
@@ -10,6 +10,7 @@
 import sys
 import json
 import os
+import shutil
 from typing import Tuple
 from scripts.windows.windows import BaseWindowsControl
 from scripts.prettyCode.prettyPrint import PrettyPrint
@@ -31,9 +32,11 @@ class Update():
         # 生成现在文件夹MD5
         PRETTYPRINT.pPrint('正在生成 OPSVN GAME SCRIPT MD5')
         OPSVNFileMd5List = []
-        for file in os.listdir('.\script\game'):
+        for file in os.listdir('.\scripts\game'):
             if file != self.myselfFile:
-                with open('.\script\\game\\{}'.format(file), 'r', encoding='utf-8') as f:
+                path = '.\scripts\\game\\{}'.format(file)
+                PRETTYPRINT.pPrint('OPSVN MD5 -> {}'.format(path))
+                with open(path, 'r', encoding='utf-8') as f:
                     i = f.read()
                 OPSVNDataMd5 = BaseWindowsControl.useMd5(i, 'file')
                 OPSVNFileMd5List.append(OPSVNDataMd5)
@@ -42,30 +45,32 @@ class Update():
         PRETTYPRINT.pPrint('正在生成 JX3 INTERFACE OPSVN SCRIPT MD5')
         GameFileMd5List = []
         for file in os.listdir(self.path):
+            PRETTYPRINT.pPrint('INTERFACE MD5 -> {}'.format(file))
             with open(os.path.join(self.path, file), 'r', encoding='utf-8') as f:
                 k = f.read()
             gameDataMd5 = BaseWindowsControl.useMd5(k, 'file')
             GameFileMd5List.append(gameDataMd5)
         
-        return (OPSVNDataMd5, GameFileMd5List)
+        return (OPSVNFileMd5List, GameFileMd5List)
         
-    def dispatch(self, gamePlay,*args, **kwargs) -> None:
+    def dispatch(self, *args, **kwargs) -> None:
         JX3InterfaceOPSVNDirMd5, nowOPSVNDirMd5 = self.checkAllFileMd5()
         if sorted(JX3InterfaceOPSVNDirMd5) != sorted(nowOPSVNDirMd5):
             PRETTYPRINT.pPrint('lua script 有新的变动，准备更新')
             # 更新
-            os.removedirs(self.path)
+            shutil.rmtree(self.path)
             PRETTYPRINT.pPrint('更新 -> 已删除 {}'.format(self.path))
             os.makedirs(self.path)
             PRETTYPRINT.pPrint('更新 -> 已创建 {}'.format(self.path))
             for file in os.listdir(r'.\scripts\game'):
-                if file != 'update.py':
+                if file != self.myselfFile:
                     command = 'copy .\scripts\game\{} {}'.format(file, self.path)
                     BaseWindowsControl.consoleExecutionWithRun(command)
-                    PRETTYPRINT.pPrint('更新 -> 已复制 {}'.format(file))
-    
+                    PRETTYPRINT.pPrint('更新 -> 已复制 {}'.format(file)) 
+        else:
+             PRETTYPRINT.pPrint('lua script 无新的变动')
+
+
 if __name__ == '__main__':
-    # obj = Update()
-    # obj.dispatch()
-    import sys
-    print(sys.argv[0].split('\\')[-1])
+    obj = Update()
+    obj.dispatch()
