dea64fca5e78d90d528dd921aa26206482f1651a
CamilleFerros <80734960+CartmanORCamille@users.noreply.github.com>
2021-07-14 17:01:09 +0800

    修复当路径不存在时PerfMon无法启动缺陷 增加PerfMon运行指定时长

--- scripts/dataMining/miner.py
+++ scripts/dataMining/miner.py
@@ -27,10 +27,17 @@ class PerfMon():
     def command(self, uid, version, *args, **kwargs):
         # use PerfMon2.x
         resultDirPath = os.path.join('..', '..', 'caches', 'memoryLeak', uid, version)
-        if not os.path.isdir(resultDirPath):
-            os.makedirs(resultDirPath)
-        baseCommand = 'PerfMon --perf_proc="JX3ClientX64.exe" --perf_prefix="JX3_" --perf_dir="{}" --perf_d3dhookdll="d3d11hook.dll"'.format(resultDirPath)
-        return baseCommand
+        resultDirPathExists = BaseWindowsControl.whereIsTheDir(os.path.join(
+            '.', 'caches', 'memoryLeak', uid, version
+        ), True)
+        if resultDirPathExists:
+            if not os.path.isdir(resultDirPath):
+                os.makedirs(resultDirPath)
+            baseCommand = 'PerfMon --perf_proc="JX3ClientX64.exe" --perf_prefix="JX3_" --perf_dir="{}" --perf_d3dhookdll="d3d11hook.dll"'.format(resultDirPath)
+            
+            return baseCommand
+        else:
+            raise FileNotFoundError('路径不存在')
 
     def _perfMonProcess(self, command):
         p = Process(target=self.runPerfMon, name='PerfMon', args=(command, ))
@@ -38,22 +45,23 @@ class PerfMon():
 
     def runPerfMon(self, command):
         subResult = BaseWindowsControl.consoleExecutionWithPopen(command, cwd=r'.\tools\PerfMon_2.12')
-        error = subResult.read().decode('gbk')
-        if error:
-            raise IOError(error)
+        subResult.communicate()
 
-    def dispatch(self, uid, version):
+    def dispatch(self, uid, version, recordTime):
+        shutdownTime = time.time() + recordTime
         command = self.command(uid, version)
         processObj = self._perfMonProcess(command)
         PRETTYPRINT.pPrint('开始采集，Start -> PerfMon')
         processObj.start()
         
         while 1:
+            # 计时
+            nowTime = time.time()
             # 检查游戏进程是否存在
             exists = ProcessMonitoring.dispatch()
             if not processObj.is_alive():
                 PRETTYPRINT.pPrint('PerfMon未知原因退出，URGENT级错误，需要立即核查', level='ERROR', bold=True)
-            if not exists and processObj.is_alive():
+            if not exists and processObj.is_alive() or shutdownTime <= nowTime:
                 PRETTYPRINT.pPrint('结束采集，kill -> PerfMon')
                 processObj.terminate()
                 break
@@ -66,7 +74,7 @@ class PerfMon():
                 PRETTYPRINT.pPrint('数据文件名更换: {} -> {}'.format(oldFile, newFile))
                 # 等待写入
                 PRETTYPRINT.pPrint('数据写入中 -> \r')
-                BaseWindowsControl.loading(3)
+                BaseWindowsControl.loading(10)
                 os.rename(oldFile, newFile)
                 PRETTYPRINT.pPrint('数据已反馈')
                 return newFile
@@ -78,3 +86,4 @@ if __name__ == '__main__':
     perfmonMiner = PerfMon()
     uid = 'ALPHA_16239165704530177'
     filePath = perfmonMiner.dispatch(uid, '940902')
+    # perfmonMiner.command(uid, '123')
