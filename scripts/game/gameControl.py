a203750897906e157bb16c6a6f10d2f913d00102
CamilleFerros <80734960+CartmanORCamille@users.noreply.github.com>
2021-07-17 18:42:58 +0800

    更新debug函数
    Signed-off-by: CamilleFerros <80734960+CartmanORCamille@users.noreply.github.com>

--- scripts/game/gameControl.py
+++ scripts/game/gameControl.py
@@ -10,6 +10,7 @@
 import json
 import time
 from scripts.prettyCode.prettyPrint import PrettyPrint
+from scripts.windows.windows import BaseWindowsControl
 
 
 PRETTYPRINT = PrettyPrint()
@@ -18,14 +19,21 @@ PRETTYPRINT = PrettyPrint()
 def debugGameControl():
     i = 0
     while 1:
-        PRETTYPRINT.pPrint('=========================DEBUG - 游戏内操作=========================')
+        PRETTYPRINT.pPrint('=========================DEBUG - 游戏内操作 -> {}========================='.format(i))
         if i == 5:
             with open(r'.\caches\GameStatus.json', 'w', encoding='utf-8') as f:
                 data = {'orReady': 1}
                 json.dump(data, f, indent=4)
+        if i == 20:
+            # 关闭游戏
+            processName = 'JX3ClientX64.exe'
+            PRETTYPRINT.pPrint('尝试结束游戏')
+            BaseWindowsControl.killProcess(processName)
+            break
         i += 1
         time.sleep(3)
 
 
 if __name__ == '__main__':
-    debugGameControl()
+    # debugGameControl()
+    BaseWindowsControl.killProcess('JX3ClientX64.exe')
