7514a9a2bed0417553582db0c97300aa90703319
CamilleFerros <80734960+CartmanORCamille@users.noreply.github.com>
2021-07-22 11:12:07 +0800

    【新增】半自动debug测试函数
    Signed-off-by: CamilleFerros <80734960+CartmanORCamille@users.noreply.github.com>

--- scripts/game/gameControl.py
+++ scripts/game/gameControl.py
@@ -16,6 +16,29 @@ from scripts.windows.windows import BaseWindowsControl
 PRETTYPRINT = PrettyPrint()
 
 
+class GameControl():
+    def __init__(self, *args, **kwargs) -> None:
+        with open(r'.\config\case.json', 'r', encoding='utf-8') as f:
+            self.controlConfig = json.load(f)
+        self.sumiAutoCaseTime = self.controlConfig.get('Debug').get('ClientSurvivalTime')
+        self.processName = 'JX3ClientX64.exe'
+
+    def autoMonitorControl(self):
+        pass
+
+    def semiAutoMaticDebugControl(self):
+        i = 0
+        while 1:
+            PRETTYPRINT.pPrint('=========================SEMI-AUTOMATIC DEBUG - 游戏内操作=========================')
+            PRETTYPRINT.pPrint('客户端已存活时间（秒）: {}，案例时间: {}'.format(i, self.sumiAutoCaseTime))
+            if i >= self.sumiAutoCaseTime:
+                break
+            i += 1
+            time.sleep(1)
+        
+        PRETTYPRINT.pPrint('客户端存活时间结束，尝试结束游戏')
+        BaseWindowsControl.killProcess(self.processName)
+
 def debugGameControl():
     i = 0
     while 1:
@@ -24,14 +47,15 @@ def debugGameControl():
             with open(r'.\caches\GameStatus.json', 'w', encoding='utf-8') as f:
                 data = {'orReady': 1}
                 json.dump(data, f, indent=4)
-        if i == 20:
+        if i == 10:
             # 关闭游戏
             processName = 'JX3ClientX64.exe'
             PRETTYPRINT.pPrint('尝试结束游戏')
             BaseWindowsControl.killProcess(processName)
             break
         i += 1
-        time.sleep(3)
+
+        time.sleep(1)
 
 
 if __name__ == '__main__':
